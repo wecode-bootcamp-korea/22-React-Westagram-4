d6b0965e7414fc0da02523db9db11f1acca7aee9
soojeonglee <eee.soojeong@gmail.com>
2021-06-23 18:10:45 +0900

    Add: first commit. 초기 세팅 완료 3차

--- src/Routes.js
+++ src/Routes.js
@@ -10,26 +10,25 @@ import LoginJaehyun from './pages/jaehyunLee/Login/Login';
 import MainJaehyun from './pages/jaehyunLee/Main/Main';
 
 // 수정's 컴포넌트
-import LoginSoojeong from './pages/SoojeongLee/Login/Login';
-import MainSoojeong from './pages/SoojeongLee/Main/Main';
+import LoginSoojeong from './pages/soojeongLee/Login/Login';
+import MainSoojeong from './pages/soojeongLee/Main/Main';
 
 // 공용 컴포넌트
-
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path='/login-Gyeongmin' component={LoginGyeongmin} />
-          <Route exact path='/main-Gyeongmin' component={MainGyeongmin} />
-          <Route exact path='/login-Jaehyun' component={LoginJaehyun} />
-          <Route exact path='/main-Jaehyun' component={MainJaehyun} />
-          <Route exact path='/login-Soojeong' component={LoginSoojeong} />
-          <Route exact path='/main-Soojeong' component={MainSoojeong} />
+          <Route exact path="/login-Gyeongmin" component={LoginGyeongmin} />
+          <Route exact path="/main-Gyeongmin" component={MainGyeongmin} />
+          <Route exact path="/login-Jaehyun" component={LoginJaehyun} />
+          <Route exact path="/main-Jaehyun" component={MainJaehyun} />
+          <Route exact path="/login-Soojeong" component={LoginSoojeong} />
+          <Route exact path="/main-Soojeong" component={MainSoojeong} />
         </Switch>
       </Router>
-    )
+    );
   }
 }
 
