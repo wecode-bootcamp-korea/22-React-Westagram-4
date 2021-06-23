efb5db274838247037e4ae6f93dd38c8f515d9cd
soojeonglee <eee.soojeong@gmail.com>
2021-06-23 18:38:14 +0900

    Add: first commit. 초기 세팅 완료 5차

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
 
+// 스타일
+import '../src/styles/reset.scss';
+import '../src/styles/common.scss';
+import '../src/components/Nav/Nav.scss';
+
 ReactDOM.render(
   <React.StrictMode>
     <Routes />
--- src/pages/jaehyunLee/Main/Main.js
+++ src/pages/jaehyunLee/Main/Main.js
@@ -4,11 +4,6 @@ import React from 'react';
 // 컴포넌트
 import Nav from '../../../components/Nav/Nav';
 
-// 기타
-import '../../../styles/reset.scss';
-import '../../../styles/common.scss';
-import '../../../components/Nav/Nav.scss';
-
 class Main extends React.Component {
   render() {
     return (
