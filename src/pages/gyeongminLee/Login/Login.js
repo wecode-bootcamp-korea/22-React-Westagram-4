d1b3d75d562d82dd12e2a473c4e9a009a7c4a5ac
glorious-min <70811550+glorious-min@users.noreply.github.com>
2021-06-25 16:49:53 +0900

    경민 임시저장

--- src/pages/gyeongminLee/Login/Login.js
+++ src/pages/gyeongminLee/Login/Login.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
-
+import './Login.scss';
 class Login extends Component {
   render() {
-    return <div>로그인</div>;
+    return (
+      <>
+        <div className="container">
+          <form id="logo" action="index.html" method="post">
+            <a id="logo">westagram</a>
+          </form>
+
+          <form id="login-form">
+            <input
+              type="text"
+              className="text"
+              id="id"
+              name="id"
+              placeholder="전화번호,사용자 이름 또는 이메일"
+            />
+
+            <input
+              type="password"
+              className="text"
+              id="password"
+              placeholder="비밀번호"
+            />
+
+            <button type="button" id="loginButton" disabled>
+              로그인
+            </button>
+          </form>
+
+          <div id="askinghelp">
+            <a href="#">비밀번호를 잊어버리셨나요?</a>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
