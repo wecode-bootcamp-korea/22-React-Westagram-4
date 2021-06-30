2bdb746dc5a55e3c5bd95ffebc4b95e3fd3adb64
glorious-min <70811550+glorious-min@users.noreply.github.com>
2021-06-30 09:23:52 +0900

    로그인 페이지 비동기 문제 해결 + 리팩토링 진행중

--- src/pages/gyeongminLee/Login/Login.js
+++ src/pages/gyeongminLee/Login/Login.js
@@ -12,13 +12,24 @@ class Login extends Component {
 
   controlValue = event => {
     if (event.target.type === 'text') {
-      this.setState({
-        idValue: event.target.value,
-      }); // 실행이 완료될때까지 기다리지 ㅇ낳는다 - setState -> 비동기 아이디 밸류 달라지기 전에 아래 함수 실행함
+      // 해결함 !!!!!
+      //인풋이 여러개일때 이벤트 객체를 활용, 타입이 텍스트인 인풋을 찾는것
+      // <form id="login-form" onChange={this.controlValue()}> 라고 적혀있으면 컨트롤 밸류의 리턴값을 찾는데 없으므로 언디파인이나온다.
+      this.setState(
+        {
+          idValue: event.target.value,
+        },
+        () => this.controlActivation(event) //정리해야할것 - 기존의 컨트롤 액티베이션 함수를 태그에 넣을 필요없이
+      );
+
+      // 실행이 완료될때까지 기다리지 않는다 - setState -> 비동기 아이디 밸류 달라지기 전에 아래 함수 실행함
     } else if (event.target.type === 'password') {
-      this.setState({
-        passwordValue: event.target.value,
-      });
+      this.setState(
+        {
+          passwordValue: event.target.value,
+        },
+        () => this.controlActivation(event)
+      );
     }
   };
 
@@ -38,31 +49,31 @@ class Login extends Component {
   };
 
   render() {
-    console.log(this.setState);
+    //console.log(this.setState);
     return (
       <>
         <div className="container">
           <form id="logo" action="index.html" method="post">
             <a id="logo">westagram</a>
           </form>
-
-          <form id="login-form" onKeyUp={this.controlActivation}>
+          {/* // 키업의 경우 손가락 오래 눌렀다 떼서 확인 가능 */}
+          <form id="login-form" onChange={this.controlValue}>
+            {' '}
+            {/* 온체인지로 바꾸기  */}
             <input
-              onChange={this.controlValue} // controlValue()
+              //onChange={this.controlValue} // controlValue()
               type="text"
               className="text"
               id="id"
               placeholder="전화번호,사용자 이름 또는 이메일"
             />
-
             <input
-              onChange={this.controlValue}
+              //onChange={this.controlValue}
               type="password"
               className="text"
               id="password"
               placeholder="비밀번호"
             />
-
             <button
               style={{ backgroundColor: this.state.buttonColor }} // 클래스 네임을 동적으로 바꾸어주자
               type="button"
--- src/pages/gyeongminLee/Main/Comment/Comment.js
+++ src/pages/gyeongminLee/Main/Comment/Comment.js
@@ -1,5 +1,42 @@
 import React from 'react';
+//import COMMENT from './commentData';
+
 class Comment extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     commentList: [],
+  //     commentValue: '',
+  //   };
+  // }
+
+  // componentDidMount() {
+  //   this.setState({
+  //     commentList: COMMENT,
+  //   });
+  // }
+
+  // handleCommentValue = e => {
+  //   this.setState({
+  //     commentValue: e.target.value,
+  //   });
+  // };
+
+  // addComment = e => {
+  //   e.preventDefault();
+  //   const { commentList, commentValue } = this.state;
+  //   this.setState({
+  //     commentList: [
+  //       ...commentList,
+  //       {
+  //         id: commentList.length + 1,
+  //         content: commentValue,
+  //       },
+  //     ],
+  //     commentValue: '',
+  //   });
+  // };
+
   render() {
     //console.log(this.props.comment);
     return (
